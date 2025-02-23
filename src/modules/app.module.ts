d52e610d2b3aa5ed19ebadfc0eceb00a2ec5b431
peperaiovinicius@gmail.com <seuemail@example.com>
2025-02-23 10:13:17 -0300

    Corrigindo erros

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,22 +1,43 @@
-import { Module } from '@nestjs/common';
+import { Controller, Get, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users.module';
-import { AuthModule } from './auth.module';
+
+@Controller()
+export class AppController {
+  @Get()
+  getRoot(): string {
+    return 'Hello, World!';
+  }
+}
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '123',
-      database: 'user_management',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        console.log('Database Configurations:');
+        console.log('Host:', process.env.TYPEORM_HOST);
+        console.log('Port:', process.env.TYPEORM_PORT);
+        console.log('Username:', process.env.TYPEORM_USERNAME);
+        console.log('Password:', process.env.TYPEORM_PASSWORD);
+        console.log('Database:', process.env.TYPEORM_DATABASE);
+        return {
+          type: 'postgres',
+          host: process.env.TYPEORM_HOST,
+          port: parseInt(process.env.TYPEORM_PORT || '5432', 10),
+          username: process.env.TYPEORM_USERNAME,
+          password: process.env.TYPEORM_PASSWORD,
+          database: process.env.TYPEORM_DATABASE,
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
+        };
+      },
     }),
     UsersModule,
-    AuthModule,
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
